38343840664ff41b8a6757ebfc46c2367e115391
xBalbinus <xiangan9@gmail.com>
2025-02-26 11:20:19 -0700

    init: test verify, ed25519

---
+++ test/TestED25519.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.6.8;
+
+import "../src/libraries/ED25519.sol";
+
+contract TestEd25519 {
+    function test_verify(
+        bytes32 k,
+        bytes32 r,
+        bytes32 s,
+        bytes memory m
+    ) public pure returns (bool) {
+        return Ed25519.verify(k, r, s, m);
+    }
+}
