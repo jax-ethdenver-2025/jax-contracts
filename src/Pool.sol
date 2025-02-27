26b16459a93dcac9322ff277c9960a33d6b631a0
xBalbinus <xiangan9@gmail.com>
2025-02-26 18:11:40 -0700

    reward pool + factory

--- src/Pool.sol
+++ src/Pool.sol
@@ -1,14 +1,38 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.28;
 
-import {Initializable} from "solady/utils/Initializable.sol";
+import {ERC20} from "solady/tokens/ERC20.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
 
-contract Pool is Initializable {
-    constructor() {
-      _disableInitializers();
+contract RewardPool is Ownable {
+    ERC20 public jaxToken;
+    uint256 public bountyPerEpoch;
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public rewards;
+
+    event Deposit(address indexed user, uint256 amount);
+    event RewardDistributed(address indexed user, uint256 reward);
+
+    constructor() {}
+
+    function initialize(address _jaxToken) external {
+        jaxToken = ERC20(_jaxToken);
     }
 
-    function initialize() external {
-      // TODO: Not implemented yet
+    function deposit(uint256 amount) external {
+        require(jaxToken.transferFrom(msg.sender, address(this), amount), "Deposit failed");
+        balances[msg.sender] += amount;
+        emit Deposit(msg.sender, amount);
+    }
+
+    function setBountyPerEpoch(uint256 _bounty) external onlyOwner {
+        bountyPerEpoch = _bounty;
+    }
+
+    function distributeRewards() external {
+        // Implement AVS logic here
+        // For each user, calculate their reward based on AVS
+        // Update their rewards mapping
+        // Emit RewardDistributed event
     }
 }
